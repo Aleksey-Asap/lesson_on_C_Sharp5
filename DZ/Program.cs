c33c782c94ecd51731832fa27b3f290a569d6ccc
Aleksey <aleksey-asapov@mail.ru>
2022-06-26 16:40:46 +0300

    Домашнее задание

---
+++ DZ/Program.cs
@@ -0,0 +1,105 @@
+﻿// Домашнее задание:
+// ЗаданиеПример Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// void Print(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[] massif = new int[7];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(100, 999);
+// }
+
+// Print(massif);
+
+// int count = 0;
+// int CountEven(int[] collection)
+// {
+//     for (int i = 0; i < collection.Length; i++)
+//     {
+//         if (collection[i] % 2 == 0) count++;
+//     }
+//     return count;
+// }
+
+// CountEven(massif);
+// Console.WriteLine($"Количество четных чисел в массиеве: {count}");
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19 
+// [-4, -6, 89, 6] -> 0
+
+// void Print(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[] massif = new int[5];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(0, 10);
+// }
+
+// Print(massif);
+
+// int sum = 0;
+// int SumUnEvenPos(int[] collection)
+// {
+//     for (int i = 1; i < collection.Length; i+= 2)
+//     {
+//         sum += collection[i];
+//     }
+//     return sum;
+// }
+
+// SumUnEvenPos(massif);
+// Console.WriteLine($"Количество четных чисел в массиеве: {sum}");
+
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным 
+// и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+// void Print(double[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// double[] massif = new double[5];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = Math.Round(new Random().NextDouble(), 1);
+// }
+
+// Print(massif);
+
+// double max = 0;
+// double min = 1;
+
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     if (massif[i] > max) max = massif[i];
+//     if (massif[i] < min) min = massif[i];
+// }
+// Console.WriteLine($"Max: {max}  Min: {min}");
+// Console.WriteLine("Разница между Max и Min:" + " " + (max - min));
---
+++ Seminar_massif/Program.cs
@@ -0,0 +1,214 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива. 
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, 
+// сумма отрицательных равна -20.
+
+// int[] massif = new int[12];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(-9, 10);
+// }
+
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     Console.Write(massif[i] + " ");
+// }
+// Console.WriteLine();
+
+// int sumPos = 0;
+// int sumNeg = 0;
+
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     if (massif[i] > 0)
+//     {
+//         sumPos += massif[i];
+//     }
+//     else
+//     {
+//         sumNeg += massif[i];
+//     }
+// }
+
+// Console.WriteLine("Положительные: " + sumPos);
+// Console.WriteLine("Отрицательные: " + sumNeg);
+
+
+// Способ 2
+// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива. 
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, 
+// сумма отрицательных равна -20.
+//
+// void Print(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+//     Console.WriteLine(); 
+// }
+// int[] massif = new int[12];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(-9, 10);
+// }
+// Print(massif);
+
+// int sumPos = 0;
+// int sumNeg = 0;
+
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     if (massif[i] > 0)
+//     {
+//         sumPos += massif[i];
+//     }
+//     else
+//     {
+//         sumNeg += massif[i];
+//     }
+// }
+
+// Console.WriteLine("Положительные: " + sumPos);
+// Console.WriteLine("Отрицательные: " + sumNeg);
+
+
+
+
+
+// Задача 32: Напишите программу замена элементов массива: положительные 
+// элементы замените на соответствующие отрицательные, и наоборот. 
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// void Print(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+//     Console.WriteLine(); 
+// }
+// int[] massif = new int[12];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(-9, 10);
+// }
+// Print(massif);
+
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] *= -1;
+// }
+
+// Print(massif);
+
+
+// Задача 33: Задайте массив. 
+// Напишите программу, которая определяет, присутствует 
+// ли заданное число в массиве. 
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// void Print(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+//     Console.WriteLine(); 
+// }
+// int[] massif = new int[8];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(0, 10);
+// }
+// Print(massif);
+
+// bool exist = false;
+
+// Console.WriteLine("Введите число:");
+// int A = int.Parse(Console.ReadLine());
+
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     if (massif[i] == A)
+//     {
+//         exist = true;
+//         break;
+//     }
+// }
+// if (exist == true) Console.WriteLine("Элемент найден");
+// else Console.WriteLine("Элемент не найден");
+
+
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. 
+// В своём решении сделайте для 
+// 123 [5, 18, 123, 6, 2] -> 1 
+// [1, 2, 3, 6, 2] -> 0 
+// [10, 11, 12, 13, 14] -> 5
+
+// // void Print(int[] arr)
+// // {
+// //     for (int i = 0; i < arr.Length; i++)
+// //     {
+// //         Console.Write(arr[i] + " ");
+// //     }
+// //     Console.WriteLine(); 
+// // }
+
+// int[] massif = new int[123];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(0, 200);
+// }
+
+// // Print(massif);
+
+// int count = 0;
+
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     if (massif[i] >= 10 & massif[i] <= 99) count++;
+// }
+
+// Console.WriteLine($" Число эллементов массива, входящих в отрезок (10,99): {count}");
+
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве. 
+// [1 2 3 4 5] -> 5 8 3 
+// [6 7 3 6] -> 36 21
+
+// void Print(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+//     Console.WriteLine(); 
+// }
+
+// int[] massif = new int[9];
+// for (int i = 0; i < massif.Length; i++)
+// {
+//     massif[i] = new Random().Next(0, 10);
+// }
+
+// Print(massif);
+
+// int [] mass = new int[(1 + massif.Length) /2];
+
+// for (int i = 0; i < mass.Length; i++)
+// {
+//     mass[i]= massif[i] * massif[massif.Length - i- 1];
+// }
+
+// if (massif.Length % 2 == 1) mass[mass.Length -1] = massif[massif.Length / 2];
+// Print(mass);
